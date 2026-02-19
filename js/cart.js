b12399789f6387e78d876132c6ecdecacbec453a
adityaraj15122004-maker <adityaraj15122004@gmail.com>
2026-02-19 15:58:25 +0530

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,12 @@
+function addToCart(id) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    const product = products.find(p => p.id === id);
+
+    cart.push(product);
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert("Product Added To Cart");
+}
 
