035244e6f8306bcd3d0b718a71500b863204f6ef
adityaraj15122004-maker <adityaraj15122004@gmail.com>
2026-02-19 15:58:46 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+const productList = document.getElementById("product-list");
+
+if (productList) {
+    products.forEach(product => {
+        productList.innerHTML += `
+            <div class="product">
+                <img src="${product.image}">
+                <h3>${product.name}</h3>
+                <p>₹${product.price}</p>
+                <a href="pages/product.html?id=${product.id}">
+                    <button>View Details</button>
+                </a>
+            </div>
+        `;
+    });
+}
 
